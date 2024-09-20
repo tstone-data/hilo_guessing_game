b6cd054816589dfb0cb21cc35c04a3697e5df449
Taylor Stone <168595554+tstone-data@users.noreply.github.com>
2024-09-20 06:27:34 -0400

    Add files via upload
    - Fixed issue with print statement
    - Added "Number of Guesses" feature

--- HiLo.java
+++ HiLo.java
@@ -8,18 +8,21 @@ public class HiLo {
 	String playAgain = "";
 	do {
 		// Generates random number between 1 and 100
-		int theNumber = (int)(Math.random() * 100 + 1);
+		int theNumber = (int)(Math.random() * 100 - 1);
 		int guess = 0;
+		int numberOfTries = 0;
 		while (guess != theNumber) {
 			System.out.println("Guess a number between 1 and 100: ");
 			guess = scan.nextInt();
 			if (guess > theNumber)
-				System.out.print(guess + " is too high, try again.");
+				System.out.println(guess + " is too high, try again.");
 			else if (guess < theNumber)
 				System.out.println(guess + " is too low, try again.");
 			else
 				System.out.println(guess + " is correct, you win!");
+			numberOfTries += 1;
 			} // End of the while loop for guessing
+			System.out.println("You guessed the number in " + numberOfTries + " tries.");
 			System.out.println("Do you want to play again (y/n)?");
 			playAgain = scan.next();
 		} while (playAgain.equalsIgnoreCase("y")); 	
